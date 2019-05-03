d6710f52bd11bcd7bd9811944fe5e01c3ba52b1c
Thomas Källman <thokall@me.com>
2019-05-03 15:04:53 +0200

    refactor code :)

--- app.R
+++ app.R
@@ -1,31 +1,19 @@
 library(shiny)
-library(tidyverse) # dplyr is included
-library(dplyr)
-library(lubridate)
-library(data.table)
 
 ui <- fluidPage(
-  h1(textOutput("week") ),
-  fluidRow(
-    
-    column(12,
-           tableOutput('table')
-    )
+  h1(textOutput("week")),
+  fluidRow(column(12, tableOutput('table')))
   )
-)
 
-server <- function (input,output){
-  
-  kitchenURL <- fread ('https://raw.github.com/wiki/Inkimar/kitchen-service/kitchen.md')
-  
+server <- function(input,output) {
+  kitchenURL <- fread('https://raw.github.com/wiki/Inkimar/kitchen-service/kitchen.md')
   rubrik <- 'Köksvecka, för vecka'
   currentWeek <- week(Sys.time())
   comb <- paste(rubrik,currentWeek)
   thisweek <- filter(kitchenURL, week == currentWeek)
-  thisweekSelect <- select(thisweek,name1,name2)
-  
+  thisweekSelect <- select(thisweek, name1, name2)
   output$table <- renderTable(thisweekSelect)
-  output$week <- renderText( {comb} )
-}
+  output$week <- renderText(comb)
+  }
 
 shinyApp(ui = ui, server = server)
