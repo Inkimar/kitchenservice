182ea43d309056acfee256f58e9e7cbe619e3af3
Ingimar Erlingsson <inkimar@gmail.com>
2019-05-03 14:39:53 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # kitchenservice
 hobby project using R and Shiny
+
+## publishing at shinyapps.io
+https://ingimar.shinyapps.io/Kitchen-tutorial5/
+
+## fetching the csv-file from a github-account 
+https://raw.github.com/wiki/Inkimar/kitchen-service/kitchen.md
+
+## TODOS 
+
+### date = 20190503
+1. filter 'header' , remove the header and replace with xxx
+2. if the csv-file is updated (kitchen.md)  and the user updates the page at shinyapps.io - the new name is not read.
+2.1 solution ? https://shiny.rstudio.com/reference/shiny/0.14/reactiveTimer.html
+3. use the shinydashboard (https://rstudio.github.io/shinydashboard/) 
---
+++ app.R
@@ -0,0 +1,31 @@
+library(shiny)
+library(tidyverse) # dplyr is included
+library(dplyr)
+library(lubridate)
+library(data.table)
+
+ui <- fluidPage(
+  h1(textOutput("week") ),
+  fluidRow(
+    
+    column(12,
+           tableOutput('table')
+    )
+  )
+)
+
+server <- function (input,output){
+  
+  kitchenURL <- fread ('https://raw.github.com/wiki/Inkimar/kitchen-service/kitchen.md')
+  
+  rubrik <- 'Köksvecka, för vecka'
+  currentWeek <- week(Sys.time())
+  comb <- paste(rubrik,currentWeek)
+  thisweek <- filter(kitchenURL, week == currentWeek)
+  thisweekSelect <- select(thisweek,name1,name2)
+  
+  output$table <- renderTable(thisweekSelect)
+  output$week <- renderText( {comb} )
+}
+
+shinyApp(ui = ui, server = server)
